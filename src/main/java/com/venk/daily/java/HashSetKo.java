176d216ad7e50ae4781e937636129e05dc917c63
li_wk <li_wk@suixingpay.com>
2018-04-09 17:28:45 +0800

    HashSet-lab

---
+++ src/main/java/com/venk/daily/java/HashSetKo.java
@@ -0,0 +1,32 @@
+package com.venk.daily.java;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+/**
+ *
+ */
+public class HashSetKo {
+
+    /**
+     * HashSet删除null值
+     * @param args
+     */
+    public static void main(String[] args){
+        HashSet<String> strSet = new HashSet<>();
+        strSet.add("01");
+        strSet.add(null);
+        strSet.add("02");
+        strSet.add("03");
+        System.out.println(strSet);
+        strSet.remove(null);
+        System.out.println(strSet);
+        Iterator<String> iterator = strSet.iterator();
+        while(iterator.hasNext()) {
+            if(iterator.next() == null) {
+                iterator.remove();
+            }
+        }
+        System.out.println(strSet);
+    }
+}
